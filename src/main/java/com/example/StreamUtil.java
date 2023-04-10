847f020b7d24e409c8845d3c0b825c8503ab453d
marwenkouidhi <marwenkouidhi@gmail.com>
2023-04-10 16:11:36 +0100

    add StreamUtil

---
+++ src/main/java/com/example/StreamUtil.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class StreamUtil {
+
+    public static DataStream getDataStream(StreamExecutionEnvironment env, final ParameterTool params) {
+        env.getConfig().setGlobalJobParameters(params);
+        DataStream<String> dataStream = null;
+
+        if (params.has("input")) {
+            System.out.println("Executing SplitWords job with file input");
+            dataStream = env.readTextFile(params.get("input"));
+        } else if (params.has("host") && params.has("port")) {
+            System.out.println("Executing SplitWords job with socket stream");
+            dataStream = env.socketTextStream(params.get("host"), Integer.parseInt(params.get("port")));
+        } else {
+            System.out.println("Use --host and --port to execute the job with socket stream");
+            System.out.println("Use --input to execute the job with an input file");
+        }
+
+        return dataStream;
+    }
+}
