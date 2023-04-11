d7cc15678701503e2681cb16dcb2f88f78d5651a
marwenkouidhi <marwenkouidhi@gmail.com>
2023-04-11 21:20:52 +0100

    update StreamUtil.java: add taskname as param for getDataStream()

--- src/main/java/com/example/StreamUtil.java
+++ src/main/java/com/example/StreamUtil.java
@@ -6,15 +6,15 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class StreamUtil {
 
-    public static DataStream getDataStream(StreamExecutionEnvironment env, final ParameterTool params) {
+    public static DataStream getDataStream(StreamExecutionEnvironment env, final ParameterTool params, String taskName) {
         env.getConfig().setGlobalJobParameters(params);
         DataStream<String> dataStream = null;
 
         if (params.has("input")) {
-            System.out.println("Executing SplitWords job with file input");
+            System.out.println("Executing " + taskName + " task with file input");
             dataStream = env.readTextFile(params.get("input"));
         } else if (params.has("host") && params.has("port")) {
-            System.out.println("Executing SplitWords job with socket stream");
+            System.out.println("Executing " + taskName + " task with socket stream");
             dataStream = env.socketTextStream(params.get("host"), Integer.parseInt(params.get("port")));
         } else {
             System.out.println("Use --host and --port to execute the job with socket stream");
